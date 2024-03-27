93e265891e55346a814ce208c2cf5670ef81483b
asamshahadat <asamshahadat@gmail.com>
2024-03-27 19:57:00 +0600

    Portfolio Page link added on hero button

--- src/components/sections/hero/Hero.jsx
+++ src/components/sections/hero/Hero.jsx
@@ -31,7 +31,7 @@ const Hero = () => {
           <h4>Hello, I’m -</h4>
           <h2>S M Shahadat Hossain</h2>
           <p>A software engineer who uses his knowledge of programming languages, software development processes, and computer science principles to design, develop, and maintain software systems.</p>
-          <Link to="#portfolio" className="btn-style-a">Portfolio</Link>
+          <Link to="/portfolio" className="btn-style-a">Portfolio</Link>
         </div>
       </div>
     </div>
